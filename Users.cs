b646bd78952566d59c0ac6c1d5dafc77529d26f2
klaukata <kkborowy@gmail.com>
2025-02-19 21:25:55 +0100

    users class

---
+++ Users.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+class User
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public User(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+}
