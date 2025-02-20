a5d5e825c12665bb7d22b00ae8df78930c759d1d
klaukata <kkborowy@gmail.com>
2025-02-20 03:38:26 +0100

    moved some classes to Models folder

--- ApiResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-// used to deserialize json, 'recording' field in a response
-public class ApiResponse
-{
-    [JsonPropertyName("recordings")]
-    public List<Recording> Recordings { get; set; }
-}
